5eceae7f1e2f64484addbed659cb86e664ad0ec1
BobbyLin <linzhangsheng23@gmail.com>
2025-07-21 21:43:26 +0800

    chore: add ui

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,27 @@
 </script>
 
 <template>
-  <div>
-    Hello, this is the default layout!
+  <div class="flex flex-col gap-2 items-center justify-center min-h-screen">
+    <Button>
+      Default
+    </Button>
+    <Button variant="destructive">
+      Destructive
+    </Button>
+    <Button variant="outline">
+      Outline
+    </Button>
+    <Button variant="secondary">
+      Secondary
+    </Button>
+    <Button variant="ghost">
+      Ghost
+    </Button>
+    <Button variant="muted">
+      Muted
+    </Button>
+    <Button variant="tertiary">
+      Tertiary
+    </Button>
   </div>
 </template>
